6cd1966263998609e993c3f9e0c9ba8f3b9d5d47
kynhix <kristopher.t.hicks@gmail.com>
2024-11-11 14:42:04 -0500

    Main menu

--- snake.c
+++ snake.c
@@ -4,7 +4,7 @@
 
 int main() {
   init();
-  startGameLoop();
+  mainMenu();
   cleanup();
   return 0;
 }
@@ -14,7 +14,56 @@ void init() {
   noecho();             // disable echo of input characters
   curs_set(0);          // hide the cursor
   keypad(stdscr, TRUE); // enable keyboard
-  start_color();        // enable color
+  // start_color();        // enable color
+}
+
+void mainMenu() {
+  const char *choices[] = {"Play Game", "High Scores", "Settings", "Exit"};
+  const int NUM_CHOICES = 4;
+  const char *TITLE = "Main Menu";
+  int choice = 0;
+  WINDOW *win = newwin(NUM_CHOICES + 2, 40, 0, 0);
+  keypad(win, TRUE); // enable arrow keys
+
+  nodelay(win, TRUE);
+  while (1) {
+    const int c = wgetch(win);
+    switch (c) {
+    case KEY_UP:
+      if (choice == 0)
+        choice = NUM_CHOICES - 1;
+      else
+        --choice;
+      break;
+    case KEY_DOWN:
+      if (choice == NUM_CHOICES - 1)
+        choice = 0;
+      else
+        ++choice;
+      break;
+    case 10: // Enter key
+      break;
+    case 'q': // Quit on 'q'
+      goto cleanup;
+    }
+    waitNextFrame();
+    werase(win);
+    box(win, 0, 0);
+    mvwprintw(win, 0, 1, "%s", TITLE);
+    for (int i = 0; i < NUM_CHOICES; ++i) {
+      if (choice == i) {
+        wattron(win, A_REVERSE);
+        mvwprintw(win, i + 1, 3, "* %-18s", choices[i]);
+        wattroff(win, A_REVERSE);
+      } else {
+        mvwprintw(win, i + 1, 3, "%-20s", choices[i]);
+      }
+    }
+    wrefresh(win);
+  }
+
+cleanup:
+  delwin(win);
 }
 
 void cleanup() { endwin(); }
@@ -22,10 +71,10 @@ void cleanup() { endwin(); }
 void startGameLoop() {
   int x = 10, y = 10, dx = 1, dy = 0;
   while (1) {
-    clear();
+    waitNextFrame();
+    erase();
     mvaddch(y, x, 'O');
     refresh();
-    usleep(10000);
 
     int ch = getch();
     if (ch == KEY_LEFT) {
--- snake.h
+++ snake.h
@@ -1,3 +1,9 @@
+#define FRAME_RATE 33333
+#define waitNextFrame() usleep(FRAME_RATE);
+
 void init();
 void startGameLoop();
 void cleanup();
+void mainMenu();
+
+enum MoveDirection { UP, DOWN, LEFT, RIGHT };
