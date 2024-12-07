5dcc29dd7d6ba03142073fd2796f60cbcc32ae57
kynhix <kristopher.t.hicks@gmail.com>
2024-12-06 20:36:01 -0500

    Added state struct

--- snake.h
+++ snake.h
@@ -1,5 +1,6 @@
 #define FRAME_RATE 33333
-#define waitNextFrame() usleep(FRAME_RATE);
+#define waitNextFrame() usleep(FRAME_RATE)
+#define MAX_HIGH_SCORES 10
 
 void init();
 void startGameLoop();
@@ -8,3 +9,15 @@ void mainMenu();
 void gameOver(int score);
 
 enum MoveDirection { UP, DOWN, LEFT, RIGHT };
+
+typedef struct high_score_node {
+  const char *name;
+  int score;
+  struct high_score_node *next;
+  struct high_score_node *prev;
+} high_score_node;
+
+typedef struct snake_state {
+  high_score_node *high_score_head;
+  int snake_speed;
+} snake_save;
